93e560f085d480c8c83e05414eedb53b241f2769
royhung <lemanhhung1555@gmail.com>
2025-07-08 21:49:23 +0700

    kieu array vs luu y

--- vu_nguyenJS/main.js
+++ vu_nguyenJS/main.js
@@ -20,5 +20,22 @@ var tinh_tuoi = function (nam_sinh) {
 document.writeln(tinh_tuoi(2000), "<br>");
 document.writeln(tinh_tuoi(2002), "<br>");
 
+var a = [123, true, "Eimi", "kaka", "maiami"];
 
+a.pop(2);
 
+a[3] = "roladon";
+document.writeln(a);
+
+// ==> Trong JavaScript, mảng (Array) có thể chứa các phần tử khác kiểu dữ liệu, và điều này hoàn toàn hợp lệ. Cùng phân tích đoạn code bạn đưa:
+
+// javascript
+
+// var a = [123, true, "Eimi", "kaka", "maiami"];
+// 1. Dòng này tạo mảng a gồm các phần tử:
+//    Vị trí (chỉ số)	Giá trị	            Kiểu dữ liệu
+//    a[0]	            123	                number
+//    a[1]	            true	            boolean
+//    a[2]	            "Eimi"	            string
+//    a[3]	            "kaka"	            string
+//    a[4]	            "maiami"	        string
