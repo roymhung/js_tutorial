9a91465121a787af440c781d3e4498b3713ae159
royhung <lemanhhung1555@gmail.com>
2025-07-08 22:06:24 +0700

    kieu stringvs cuc ki quan trong phan note

--- vu_nguyenJS/main.js
+++ vu_nguyenJS/main.js
@@ -25,7 +25,7 @@ var a = [123, true, "Eimi", "kaka", "maiami"];
 a.pop(2);
 
 a[3] = "roladon";
-document.writeln(a);
+document.writeln(a, "<br>");
 
 // ==> Trong JavaScript, mảng (Array) có thể chứa các phần tử khác kiểu dữ liệu, và điều này hoàn toàn hợp lệ. Cùng phân tích đoạn code bạn đưa:
 
@@ -39,3 +39,17 @@ document.writeln(a);
 //    a[2]	            "Eimi"	            string
 //    a[3]	            "kaka"	            string
 //    a[4]	            "maiami"	        string
+
+var s = "hung dep trai"; // string se khong co 1 so co che dac biet nhu cua array: map(), filter(), find(),...
+document.writeln(s.length, "<br>")// length: ham nay co cach su dung giong array nhung khong phai array goi la ==> array-like object
+// ✅ Array-like object (Đối tượng giống mảng) trong JavaScript là gì?
+// Array-like objects là những đối tượng (object) không phải mảng thực sự, nhưng có:
+
+// Thuộc tính length (số lượng phần tử),
+
+// Các chỉ số dạng số (0, 1, 2,...) làm key.
+
+// ==> Tuy giống mảng về hình thức, nhưng không có đầy đủ các phương thức của mảng như .push(), .pop(), .map(), v.v.
+
+
+// ✅ Trong JavaScript, type coercion (ép kiểu tự động) là quá trình ngôn ngữ tự động chuyển đổi một giá trị từ kiểu này sang kiểu khác khi cần thiết, thường diễn ra trong các phép toán hoặc biểu thức điều kiện. Có hai dạng chính: 1. Implicit Coercion (Ép kiểu ngầm), 2. Explicit Coercion (Ép kiểu tường minh), 3. Một số “bẫy” thường gặp
