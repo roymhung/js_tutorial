1f68451cb4426ea9458bd90286dd307444c8c913
royhung <lemanhhung1555@gmail.com>
2025-07-08 21:07:08 +0700

    for loop

--- vu_nguyenJS/main.js
+++ vu_nguyenJS/main.js
@@ -1,3 +1,6 @@
-var tuoi = 18; // let, const khoi tao
-// var tuoi2 = tuoi;
+var tuoi = 0;
 
+tuoi = parseInt(window.prompt("Nhập tuổi:"));
+for(let i = 0; i < 10; i++){
+    document.writeln("Năm nay em ", tuoi + i, " tuổi.<br>");
+}
