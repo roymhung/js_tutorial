d8797700c8a4a491eb8d272d1252efdacae5691b
royhung <lemanhhung1555@gmail.com>
2025-07-08 21:27:42 +0700

    kieu function cach 1

--- vu_nguyenJS/main.js
+++ vu_nguyenJS/main.js
@@ -1,12 +1,22 @@
 var tuoi = 0;
 
 let co_hang_xom = {
-    ten : "lan",
-    tuoi: 18,
-    hon_nhan: "chua",
-    dia_chi: {
-        quan: "cau giay",
-        phuong: "mai dich",
-    }
-}
-document.writeln(co_hang_xom.hon_nhan)
+  ten: "lan",
+  tuoi: 18,
+  hon_nhan: "chua",
+  dia_chi: {
+    quan: "cau giay",
+    phuong: "mai dich",
+  },
+};
+// document.writeln(co_hang_xom.hon_nhan);
+
+var tinh_tuoi = function () {
+  var nam_sinh = parseInt(window.prompt("Nhập năm sinh: "));
+  tuoi = 2025 - nam_sinh;
+  document.writeln(tuoi, "<br>");
+};
+
+tinh_tuoi();
+tinh_tuoi();
+
