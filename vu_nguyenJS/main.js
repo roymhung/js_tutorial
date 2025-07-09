5c905c3012e0d726f74daedf3115b0982daf6e05
royhung <lemanhhung1555@gmail.com>
2025-07-09 18:31:48 +0700

    async vs await

--- vu_nguyenJS/main.js
+++ vu_nguyenJS/main.js
@@ -1,5 +1,17 @@
-var xin_chao = function(){
-  var ten = document.getElementById("input").value;
-  var loi_chao = "Xin chào " + ten;
-  document.getElementById("loi_chao").innerHTML = loi_chao;
+// var xin_chao = function(){
+//   var ten = document.getElementById("input").value;
+//   var loi_chao = "Xin chào " + ten;
+//   document.getElementById("loi_chao").innerHTML = loi_chao;
+// }
+
+//async await
+
+var thong_bao = function() {
+  document.getElementById("loi_chao").innerHTML += "Hết giớ! <br>";
+
+  
 }
+
+setTimeout(thong_bao, 3000);
+
+document.getElementById("loi_chao").innerHTML += "Xin chào! <br>";
